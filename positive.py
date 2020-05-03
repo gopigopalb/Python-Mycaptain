c1eeddf29e10c7767aeac7b33e49a80f157ab802
gopigopalb <56151751+gopigopalb@users.noreply.github.com>
2020-05-03 19:12:09 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,15 @@
+#using for loop
+list1 = [12,-7,5,64,-14]
+for num in list1 :
+  if num >= 0 :
+   print(num,end = ",")
+   
+   
+   
+   #using while loop
+list1 = [-10, 21, -4, -45, -66, 93] 
+num = 0
+while(num < len(list1)):  
+    if list1[num] >= 0: 
+        print(list1[num], end = " ")
+    num += 1
